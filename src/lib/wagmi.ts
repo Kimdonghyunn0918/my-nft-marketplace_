2e54238084db507510969207d715ef51f4ecdbab
dongmuel02 <kdh200022@gmail.com>
2025-12-10 00:40:39 +0900

    Update wagmi.ts

---
+++ src/lib/wagmi.ts
@@ -0,0 +1,10 @@
+'use client';
+
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { sepolia } from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'My NFT Marketplace',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [sepolia],
+});
